b1e59fa627bb8dbac321cb5315ea92fa1a3009c8
JiwooLee <ckdn9595@gmail.com>
2024-06-23 22:51:51 +0900

    db update

--- db/database.py
+++ db/database.py
@@ -1,22 +1,35 @@
 import sqlite3
 
-# 전역 연결 객체와 커서 초기화
-con = sqlite3.connect('./main.db')
-cur = con.cursor()
+# SQLite 연결 생성 함수
+@st.experimental_singleton
+def get_connection():
+    return sqlite3.connect('./main.db', check_same_thread=False)
+
+# 데이터베이스 초기화
 
 
 def init():
+    con = get_connection()
+    cur = con.cursor()
     cur.execute(
         "CREATE TABLE IF NOT EXISTS CaseInfo(UUID text, Summary text, AgreeList text, Person1 text, Person2 text);")
     con.commit()
 
+# 데이터 삽입
 
-def insert(uuid, summary, agreeList, person1, person2):
+
+def insert(uuid, summary, agree_list, person1, person2):
+    con = get_connection()
+    cur = con.cursor()
     cur.execute('INSERT INTO CaseInfo VALUES(?, ?, ?, ?, ?);',
-                (uuid, summary, agreeList, person1, person2))
+                (uuid, summary, agree_list, person1, person2))
     con.commit()
+
+# 데이터 조회
 
 
 def read(uuid):
+    con = get_connection()
+    cur = con.cursor()
     cur.execute('SELECT * FROM CaseInfo WHERE UUID = ? LIMIT 1', (uuid,))
     return cur.fetchone()
